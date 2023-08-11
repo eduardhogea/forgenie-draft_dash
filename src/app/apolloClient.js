6a4d2ff7495ac1cc6bfb7a39c50f88ac79a22fb4
Eduard <eduard.hogea00@e-uvt.ro>
2023-08-11 17:09:11 +0300

    fix: url

--- src/app/apolloClient.js
+++ src/app/apolloClient.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const APIURL = 'https://api.studio.thegraph.com/query/50950/contract/v0.1'
+const APIURL = 'https://api.studio.thegraph.com/query/50950/contract/v0.2'
 
 const client = new ApolloClient({
   uri: APIURL,
